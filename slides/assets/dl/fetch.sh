ed9435d8aeef1c6b83aba385227ab686edb46efa
Gonçalo Baltazar <me@goncalomb.com>
2020-02-13 23:49:00 +0000

    More slides and images

---
+++ slides/assets/dl/fetch.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd "$(dirname -- "$0")"
+
+get() {
+    echo "$1"
+    if [ -z "$2" ]; then
+        curl -#RLO "$1"
+    else
+        curl -#RLo "$2" "$1"
+    fi
+}
+
+get https://github.com/sinfo/ng-sinfo-webapp/raw/cb545ab469e1eb3d2224ad028bf6fa02141e43a6/src/assets/img/logo-branco2.png sinfo.png
+get https://github.com/nodejs/nodejs.org/raw/863043716aef43f9738a63140d6f31c83a0c5682/static/images/logos/nodejs-new-pantone-black.svg nodejs.svg
+get https://github.com/facebook/create-react-app/raw/4c0c81953d090dbf122e126f2b6b8c803d4a0569/packages/cra-template/template/src/logo.svg react.svg
+get https://github.com/webpack/media/raw/fd2b8ee77179061b6a0647bf98fd3f9c3acde681/logo/icon.svg webpack.svg
+get https://github.com/kubernetes/kubernetes/raw/3eec0a9a470a34de1fe89c2f6a24e1eabd22a60d/logo/logo.svg kubernetes.svg
+get https://www.docker.com/sites/default/files/d8/2019-07/Moby-logo.png docker.png
+get https://upload.wikimedia.org/wikipedia/commons/9/93/Amazon_Web_Services_Logo.svg aws.svg
--- slides/components.jsx
+++ slides/components.jsx
@@ -1,7 +1,6 @@
-/** @jsx jsx */
-import { jsx, css } from '@emotion/core';
+import styled from '@emotion/styled';
 
-const cssCenterContainer = css`
+export const CenterContainer = styled.div`
     text-align: center;
     p, pre {
         margin: 0.5em 0 !important;
@@ -11,4 +10,26 @@ const cssCenterContainer = css`
     }
 `;
 
-export const CenterContainer = (props) => <div css={cssCenterContainer} {...props} />;
+export const List = styled.ul`
+    max-width: 85%;
+    small {
+        color: grey;
+    }
+`;
+
+export const ImageTable = styled.div`
+    display: flex;
+    width: 85%;
+    text-align: center;
+    margin: 1em 0;
+    div {
+        display: flex;
+        flex-direction: column;
+        flex: 1;
+    }
+    img {
+        max-height: ${({ imgMaxHeight = "200px" }) => imgMaxHeight };
+        object-fit: fill;
+        margin: auto;
+    }
+`
